bd769a9ca3d8fde1cbd561040cf3db7fa6f74dc2
Nilay Chheda <mit2nil@gmail.com>
2016-12-23 18:42:10 -0800

    Optimized workflow scripts

--- auto_workflow/remote_auto_update_dev.sh
+++ auto_workflow/remote_auto_update_dev.sh
@@ -2,24 +2,42 @@
 
 CURR_PATH="$(pwd)"
 
+# Check empty string.
 if [ -z $1 ]; then
     echo "No such repo"
     exit
 fi
 
-# git pull
+# Check for AAC_ROOT variable
+if [ -z "$AAC_ROOT" ]; then
+	echo "AAC_ROOT not set. Halting!"
+	echo "Use 'source paths.sh' to set required paths."
+	exit
+fi
+
+# Check for spark-submit command
+ss=`which spark-submit`
+if [ -z "$ss" ]; then
+	echo "Spark-submit not in the path. Halting!"
+	echo "Use 'source paths.sh' to set required paths."
+	exit
+fi
+
+# Get latest data.
 echo "----------"
 echo "1. Synchronize data from Github"
-REPO_PATH="../../aac-repos/$1"
+REPO_PATH=$AAC_ROOT/aac-repos/$1
 if [ ! -d $REPO_PATH ]; then
-    echo "Repo directory doesn't exist"
-    exit
+    echo "Repo doesn't exist. Doing git clone..."
+    cd $AAC_ROOT/aac-repos
+	git clone https://github.com/american-art/${1}.git
+else
+	echo "Repo already exists. Doing git clean..."
+	cd $AAC_ROOT/aac-repos/$1
+	git checkout -- .
+	git clean -fd
+	git pull
 fi
-cd $REPO_PATH
-git status
-#git checkout -- .
-#git clean -fd
-#git pull
 cd $CURR_PATH
 
 # applay karma model
@@ -30,14 +48,11 @@ if [ ! -f "${CONFIG_FILE}.py" ]; then
     echo "Repo configuration file doesn't exist"
     exit
 fi
-# local version
-spark-submit --archives ~/ISI/aac-dependencies/karma.zip --py-files ~/ISI/aac-dependencies/python-lib.zip --driver-class-path ~/ISI/softwares/Web-Karma/karma-spark/target/karma-spark-0.0.1-SNAPSHOT-shaded.jar auto_workflow.py $CONFIG_FILE
-# server version
-# /opt/aac-softwares/spark-1.5.0-cdh5.5.0/bin/spark-submit --archives /opt/aac-dependencies/karma.zip --py-files /opt/aac-dependencies/python-lib.zip --driver-class-path /opt/aac-softwares/Web-Karma/karma-spark/target/karma-spark-0.0.1-SNAPSHOT-shaded.jar auto_workflow.py $CONFIG_FILE
+spark-submit --archives $AAC_ROOT/aac-dependencies/karma.zip --py-files $AAC_ROOT/aac-dependencies/python-lib.zip --driver-class-path $AAC_ROOT/aac-softwares/Web-Karma/karma-spark/target/karma-spark-0.0.1-SNAPSHOT-shaded.jar auto_workflow.py $CONFIG_FILE
 
-# import into triple store
+# import into dev triple store
 echo -e "\n----------"
-echo "3. Import into triple store"
+echo "3. Import into dev triple store (american-art-dev)"
 python auto_import.py "$1" dev
 
 echo -e "\n----------"
--- auto_workflow/remote_auto_update_pro.sh
+++ auto_workflow/remote_auto_update_pro.sh
@@ -2,9 +2,17 @@
 
 CURR_PATH="$(pwd)"
 
+# Check empty string.
 if [ -z $1 ]; then
     echo "No such repo"
     exit
+fi
+
+# Check for AAC_ROOT variable
+if [ -z "$AAC_ROOT" ]; then
+	echo "AAC_ROOT not set. Halting!"
+	echo "Use 'source <paths.sh>' to set required paths."
+	exit
 fi
 
 # push generated data into github
@@ -12,20 +20,24 @@ fi
 # the github account is american-art-admin, you need to set up ssh key first.
 echo "----------"
 echo "1. Push generated data to Github"
-REPO_PATH="../../aac-repos/$1"
+REPO_PATH=$AAC_ROOT/aac-repos/$1
 if [ ! -d $REPO_PATH ]; then
-    echo "Repo directory doesn't exist"
-    exit
+    echo "Repo doesn't exist. Nothing to push... settling with git clone"
+    cd $AAC_ROOT/aac-repos
+	git clone https://github.com/american-art/${1}.git
+else
+	echo "Repo already exists. Doing git push..."
+	cd $REPO_PATH
+	git add .
+	git commit -m "Applied updated model(s)"
+	git push
 fi
-cd $REPO_PATH
-git add .
-git commit -m "generate data"
-git push
+
 cd $CURR_PATH
 
-# import into triple store
+# import into production triple store
 echo -e "\n----------"
-echo "2. Import into triple store"
+echo "2. Import into production triple store (american-art)"
 python auto_import.py "$1" pro
 
 echo -e "\n----------"
